8c89d49cb2c31714072d9d7562c947528e4ff884
eshaft <eshaft@gmail.com>
2017-10-23 22:39:27 +0300

    vk-ads

--- server/common/config/main.php
+++ server/common/config/main.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'bootstrap' => ['gii', 'debug', 'gearman', 'beanstalkd', 'rabbitmq'],
+    'bootstrap' => ['gearman', 'beanstalkd', 'rabbitmq'],
     'components' => [
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
